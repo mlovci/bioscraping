5ad5d879e0df92dffdd0201e6f81bde5e99aa041
Michael Lovci <michaeltlovci@gmail.com>
2015-06-25 17:35:21 -0300

    Changes for PyPi

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     packages=find_packages(),
     url = 'https://github.com/mlovci/bioscraping',
     license = 'MIT',
-    author = 'lovci',
+    author = 'Michael T Lovci',
+    keywords=["scraping", "biological data"],
     author_email = 'Michael.T.Lovci@gmail.com',
     description = 'Scrape biological data from websites'
 )
