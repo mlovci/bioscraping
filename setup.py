f5bf52aa865c4ffb8bd42e7498e5aa74c276ea65
Michael Lovci <michaeltlovci@gmail.com>
2015-06-25 18:09:59 -0300

    Update setup.py
    Version 0.0.3 bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'bioscraping',
-    version = '0.0.2',
+    version = '0.0.3',
     packages=find_packages(),
     url = 'https://github.com/mlovci/bioscraping',
     license = 'MIT',
